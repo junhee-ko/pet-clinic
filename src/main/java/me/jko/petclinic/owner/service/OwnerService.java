6475878028b63c335c7bf7813ff68d6841f411a0
Junhee Ko <jko@rainist.com>
2021-02-19 00:31:30 +0900

    Add Owner service

--- src/main/java/me/jko/petclinic/owner/service/OwnerService.java
+++ src/main/java/me/jko/petclinic/owner/service/OwnerService.java
@@ -2,8 +2,15 @@ package me.jko.petclinic.owner.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import me.jko.petclinic.owner.dto.OwnerCreateDto;
+import me.jko.petclinic.owner.dto.OwnerUpdateDto;
+import me.jko.petclinic.owner.entity.Owner;
 import me.jko.petclinic.owner.repository.OwnerRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +18,25 @@ public class OwnerService {
 
   private final OwnerRepository ownerRepository;
 
+  public Owner saveOwnerBy(OwnerCreateDto ownerCreateDto) {
+    Owner owner = Owner.of(ownerCreateDto);
+    return ownerRepository.save(owner);
+  }
 
+  public List<Owner> getOwners() {
+    return ownerRepository.findAll();
+  }
+
+  @Transactional
+  public Owner updateOwnerBy(Long ownerId, OwnerUpdateDto ownerUpdateDto) {
+    Owner owner = ownerRepository.findById(ownerId).orElseThrow(EntityNotFoundException::new);
+    owner.updateBy(ownerUpdateDto);
+
+    return owner;
+  }
+
+  public void deleteOwnerBy(Long ownerId) {
+    Owner owner = ownerRepository.findById(ownerId).orElseThrow(EntityNotFoundException::new);
+    ownerRepository.delete(owner);
+  }
 }
