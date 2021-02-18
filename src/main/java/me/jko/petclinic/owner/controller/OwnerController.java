150e7f875fde1fcf2b546d55cc13f7ad3bad7429
Junhee Ko <jko@rainist.com>
2021-02-19 00:31:19 +0900

    Add Owner controller

--- src/main/java/me/jko/petclinic/owner/controller/OwnerController.java
+++ src/main/java/me/jko/petclinic/owner/controller/OwnerController.java
@@ -1,9 +1,20 @@
 package me.jko.petclinic.owner.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.jko.petclinic.owner.dto.OwnerCreateDto;
+import me.jko.petclinic.owner.dto.OwnerUpdateDto;
+import me.jko.petclinic.owner.entity.Owner;
 import me.jko.petclinic.owner.service.OwnerService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +22,23 @@ public class OwnerController {
 
   private final OwnerService ownerService;
 
+  @PostMapping("/owners")
+  public Owner createOwner(@Valid OwnerCreateDto ownerCreateDto) {
+    return ownerService.saveOwnerBy(ownerCreateDto);
+  }
 
+  @GetMapping("/owners")
+  public List<Owner> getOwners() {
+    return ownerService.getOwners();
+  }
+
+  @PutMapping("/owners/{ownerId}")
+  public Owner updateOwnerBy(@PathVariable Long ownerId, @Valid OwnerUpdateDto ownerUpdateDto) {
+    return ownerService.updateOwnerBy(ownerId, ownerUpdateDto);
+  }
+
+  @DeleteMapping("/owners/{ownerId}")
+  public void deleteOwnerBy(@PathVariable Long ownerId) {
+    ownerService.deleteOwnerBy(ownerId);
+  }
 }
